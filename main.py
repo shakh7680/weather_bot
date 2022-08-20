c43e21abe04424c2cb08b6f5ddda179e06e13761
Shakhzod Burkhonov <burkhonovshakhzod2000@gmail.com>
2022-08-20 20:54:28 +0500

    make it better

---
+++ main.py
@@ -0,0 +1,33 @@
+import logging
+import requests
+from datetime import datetime
+from aiogram import Bot, Dispatcher, executor, types
+from data import config
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Ob-havo botga xush kelibsiz\n"
+                        "Ob havoni bilish uchun shahar nomini kiriting")
+
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    await message.reply("Bu bot ob-havo bot")
+
+
+@dp.message_handler()
+async def weather(message: types.Message):
+    city = message.text
+    url = f'https://api.weatherapi.com/v1/forecast.json?key={config.API_KEY}&q={city}'
+    res_from_url = requests.get(url).json()
+    today_weather = res_from_url["current"]["temp_c"]
+    response = f'Bugun {city}da havo harodati {today_weather}\u2103 daraja.'
+    await message.reply(response)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
